e7346fcb9782709b04d1ec141494f309c5d40248
choi <tls9607@naver.com>
2023-08-22 10:17:45 +0900

    ApiResponseDto에 어노테이션 추가

--- src/main/java/com/ocho/what2do/common/dto/ApiResponseDto.java
+++ src/main/java/com/ocho/what2do/common/dto/ApiResponseDto.java
@@ -1,8 +1,14 @@
 package com.ocho.what2do.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) //응답값으로 변환이 될 때 제이슨 형태로 변환됨. 그때 NON_NULL인 얘들만 포함시킴.
 public class ApiResponseDto {
   private int statusCode;
   private String statusMessage;
