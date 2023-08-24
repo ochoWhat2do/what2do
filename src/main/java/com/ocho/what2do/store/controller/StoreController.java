3fc4ea3dcd911a67cc1d922d3cacaab3277ec81b
choi <tls9607@naver.com>
2023-08-24 19:30:25 +0900

    CustomException 하고 CustomErrorCode 클래스 를 사용하여 예외처리. swagger 적용.

--- src/main/java/com/ocho/what2do/store/controller/StoreController.java
+++ src/main/java/com/ocho/what2do/store/controller/StoreController.java
@@ -5,10 +5,12 @@ import com.ocho.what2do.common.security.UserDetailsImpl;
 import com.ocho.what2do.store.dto.StoreListResponseDto;
 import com.ocho.what2do.store.dto.StoreRequestDto;
 import com.ocho.what2do.store.dto.StoreResponseDto;
+import com.ocho.what2do.store.dto.StoreViewResponseDto;
 import com.ocho.what2do.store.entity.Store;
 import com.ocho.what2do.store.service.StoreService;
 import com.ocho.what2do.storefavorite.dto.StoreFavoriteListResponseDto;
 import com.ocho.what2do.storefavorite.dto.StoreFavoriteResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,8 +38,8 @@ public class StoreController {
     }
 
     @GetMapping("/stores/{storeId}") //가게 단건 조회
-    public ResponseEntity<StoreResponseDto> getStoreById(@PathVariable Long storeId) {
-        StoreResponseDto result = storeService.getStoreById(storeId);
+    public ResponseEntity<StoreViewResponseDto> getStoreById(@PathVariable Long storeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        StoreViewResponseDto result = storeService.getStoreById(storeId, userDetails.getUser());
         return ResponseEntity.ok().body(result);
     }
 
@@ -56,12 +58,14 @@ public class StoreController {
         return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.OK.value(), "가게 삭제 성공"));
     }
 
+    @Operation(summary = "가게 찜하기", description = "가게를 찜합니다.")
     @PostMapping("/stores/{storeId}/storefavorites") // 가게 찜하기
     public ResponseEntity<StoreFavoriteResponseDto> addStoreFavorite(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long storeId){
         StoreFavoriteResponseDto result = storeService.addStoreFavorite(storeId,userDetails.getUser());
         return ResponseEntity.ok().body(result);
     }
 
+    @Operation(summary = "가게 찜 목록 조회", description = "가게를 찜한 목록을 조회합니다.")
     @GetMapping("stores/{storeId}/storefavorites") // 가게 찜 목록 조회
     public ResponseEntity<StoreFavoriteListResponseDto> getStoreFavorite(@AuthenticationPrincipal UserDetailsImpl userDetails){
         StoreFavoriteListResponseDto result = storeService.getStoreFavorite(userDetails.getUser());
@@ -69,6 +73,7 @@ public class StoreController {
         return ResponseEntity.ok().body(result);
     }
 
+    @Operation(summary = "가게 찜하기 취소", description = "가게를 찜하기를 취소합니다.")
     @DeleteMapping("stores/{storeId}/storefavorites") //가게 찜 취소
     public ResponseEntity<ApiResponseDto> deleteStoreFavorite(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long storeId){
         storeService.deleteStoreFavorite(storeId, userDetails.getUser());
