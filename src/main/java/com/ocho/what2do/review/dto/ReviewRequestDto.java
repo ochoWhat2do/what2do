8e501fc01cfb568f55721744acb218d1fe13b258
SuengHyunLee <dltmdgus2000@naver.com>
2023-08-30 12:50:13 +0900

    [fix] 조건 추가

--- src/main/java/com/ocho/what2do/review/dto/ReviewRequestDto.java
+++ src/main/java/com/ocho/what2do/review/dto/ReviewRequestDto.java
@@ -1,6 +1,7 @@
 package com.ocho.what2do.review.dto;
 
 import com.ocho.what2do.common.file.S3FileDto;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +10,10 @@ import java.util.List;
 @Getter
 @Setter
 public class ReviewRequestDto {
-    //    @NotBlank(message = "리뷰 제목은 필수입니다.")
+    @NotBlank(message = "리뷰 제목은 필수입니다.")
     private String title;
 
-    //    @NotBlank(message = "리뷰 내용은 필수입니다.")
+    @NotBlank(message = "리뷰 내용은 필수입니다.")
     private String content;
 
     private List<S3FileDto> attachment;
