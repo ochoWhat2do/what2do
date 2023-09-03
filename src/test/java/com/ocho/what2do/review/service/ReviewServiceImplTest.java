19b357d497ada8d076c15f664d37e63880d253ba
SuengHyunLee <dltmdgus2000@naver.com>
2023-09-03 20:58:08 +0900

    [fix] 리뷰삭제테스트코드 수정

--- src/test/java/com/ocho/what2do/review/service/ReviewServiceImplTest.java
+++ src/test/java/com/ocho/what2do/review/service/ReviewServiceImplTest.java
@@ -176,12 +176,44 @@ public class ReviewServiceImplTest {
     @Order(2)
     @DisplayName("리뷰 삭제하기")
     void deleteReviewTest() throws IOException {
+
         // Given
-        String userEmail = "test01@email.com";
         String title = "타이틀1#";
+        String content = "라뷰1";
+        Long orderNo = 0L;
+        String storeKey = "-10";
+        List<Store> storeList = storeRepository.getStoreListByStoreKey(storeKey);
+        Long storeId = (long) (storeList.size() > 0 ? storeList.get(storeList.size() - 1).getId() : -1);
+        String userEmail = "test01@email.com";
+
 
         User user = userRepository.findByEmail(userEmail).orElse(null);
         assertNotNull(user);
+        ReviewResponseDto responseDto = null;
+        if (storeId > 0) {
+            ReviewRequestDto requestDto = ReviewRequestDto.builder()
+                    .title(title)
+                    .content(content)
+                    .orderNo(orderNo)
+                    .storeId(storeId)
+                    .build();
+            MockMultipartFile file1 = new MockMultipartFile("files"
+                    , "palmtree.png"
+                    , "multipart/form-data"
+                    , new FileInputStream(getClass().getResource("/image/palmtree.png").getFile())
+            );
+            MockMultipartFile file2 = new MockMultipartFile("files"
+                    , "pororo.png"
+                    , "multipart/form-data"
+                    , new FileInputStream(getClass().getResource("/image/pororo.png").getFile())
+            );
+
+            List<MultipartFile> files = List.of(
+                    file1,
+                    file2
+            );
+            reviewService.createReview(storeId, requestDto, user, null);
+        }
 
 
         // When
