c5d9c4f6a4af7f88754a87e4500c8780edc63104
Ahn Jeongmin <133615790+MI-Ryeon@users.noreply.github.com>
2023-09-05 19:44:22 +0900

    [update] 다음 API 개선 (#51)
    * [update] 에러 공통 메세지 추가
    
    * [update] dto 수정에 따른 실시간 검색 반환 타입 변경

--- src/main/java/com/ocho/what2do/common/daum/repository/ApiStoreRepository.java
+++ src/main/java/com/ocho/what2do/common/daum/repository/ApiStoreRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +18,6 @@ public interface ApiStoreRepository extends JpaRepository<ApiStore, Long> {
     Page<ApiStore> findByCategoryContains(String category, Pageable pageable);
 
     Page<ApiStore> findAll(Pageable pageable);
+
+    List<ApiStore> findAllByCategoryContains(String category);
 }
