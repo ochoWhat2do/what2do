a285b5d647ac29a4a20fa7bb689a11d961c6c655
Ahn Jeongmin <133615790+MI-Ryeon@users.noreply.github.com>
2023-09-06 18:14:54 +0900

    [refactor] 파일 리팩토링

--- src/main/java/com/ocho/what2do/store/repository/StoreRepository.java
+++ src/main/java/com/ocho/what2do/store/repository/StoreRepository.java
@@ -1,15 +1,16 @@
 package com.ocho.what2do.store.repository;
 
 import com.ocho.what2do.store.entity.Store;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
     boolean existsStoreByStoreKey(String storeKey);
+
     List<Store> getStoreListByStoreKey(String storeKey);
 
     Optional<Store> getStoreByStoreKey(String storeKey);
