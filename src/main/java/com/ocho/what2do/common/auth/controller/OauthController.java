d7af15e4895993fc47dc5c39c2cf634111f896af
SuengHyunLee <dltmdgus2000@naver.com>
2023-09-07 18:47:46 +0900

    [feature] 구글 로그인

--- src/main/java/com/ocho/what2do/common/auth/controller/OauthController.java
+++ src/main/java/com/ocho/what2do/common/auth/controller/OauthController.java
@@ -2,16 +2,9 @@ package com.ocho.what2do.common.auth.controller;
 
 import com.ocho.what2do.common.auth.service.OauthService;
 import com.ocho.what2do.user.dto.SocialUserResponseDto;
-import com.ocho.what2do.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +25,17 @@ public class OauthController {
         .header("Authorization", socialUserResponseDto.getAccessToken())
         .header("Authorization_refresh", socialUserResponseDto.getRefreshToken())
         .body(socialUserResponseDto);
+  }
+
+  @Operation(summary = "구글 로그인", description = "구글 로그인 처리.")
+  @PostMapping("/login/oauth2/code/google")
+  public ResponseEntity GoogleLogin(@RequestParam String code) {
+
+    SocialUserResponseDto socialUserResponseDto = oauthService.GoogleLogin(code);
+
+    return ResponseEntity.ok()
+            .header("Authorization", socialUserResponseDto.getAccessToken())
+            .header("Authorization_refresh", socialUserResponseDto.getRefreshToken())
+            .body(socialUserResponseDto);
   }
 }
