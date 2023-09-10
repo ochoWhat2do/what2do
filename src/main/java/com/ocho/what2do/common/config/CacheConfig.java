52a3276861806b15fddec2e19e45711f2959646d
sxi8022 <sxi8022@gmail.com>
2023-09-10 17:52:22 +0900

    [fix] 소스코드 줄맞춤

--- src/main/java/com/ocho/what2do/common/config/CacheConfig.java
+++ src/main/java/com/ocho/what2do/common/config/CacheConfig.java
@@ -17,24 +17,27 @@ import java.util.List;
 @EnableScheduling
 @Configuration
 public class CacheConfig {
-    @Bean
-    public CacheManager cacheManager() {
-        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
-        cacheManager.setAllowNullValues(true);
-        cacheManager.setCacheNames(List.of("store_all", "store_one"));
-        return cacheManager;
-    }
 
-    @Scheduled(fixedDelay = 21600000)   // 6시간 간격으로 캐시메모리 정리 (1초는 fixedDelay = 1000)
-    public void evictCache() {
-        Cache cache1 = cacheManager().getCache("store_all");
-        if (cache1 != null) {
-            cache1.clear();
-        };
-        Cache cache2 = cacheManager().getCache("store_one");
-        if (cache2 != null) {
-            cache2.clear();
-        };
-        log.info("캐시 메모리 삭제");
+  @Bean
+  public CacheManager cacheManager() {
+    ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+    cacheManager.setAllowNullValues(true);
+    cacheManager.setCacheNames(List.of("store_all", "store_one"));
+    return cacheManager;
+  }
+
+  @Scheduled(fixedDelay = 21600000)   // 6시간 간격으로 캐시메모리 정리 (1초는 fixedDelay = 1000)
+  public void evictCache() {
+    Cache cache1 = cacheManager().getCache("store_all");
+    if (cache1 != null) {
+      cache1.clear();
     }
+    ;
+    Cache cache2 = cacheManager().getCache("store_one");
+    if (cache2 != null) {
+      cache2.clear();
+    }
+    ;
+    log.info("캐시 메모리 삭제");
+  }
 }
