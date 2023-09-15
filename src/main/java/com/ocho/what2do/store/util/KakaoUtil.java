6f3f66fe8a13101eccf72c1724cac7d5fbdc0056
sxi8022 <sxi8022@gmail.com>
2023-09-16 00:46:51 +0900

    [hotfix] 소스코드 리팩토링

--- src/main/java/com/ocho/what2do/store/util/KakaoUtil.java
+++ src/main/java/com/ocho/what2do/store/util/KakaoUtil.java
@@ -3,13 +3,18 @@ package com.ocho.what2do.store.util;
 import com.ocho.what2do.common.exception.CustomException;
 import com.ocho.what2do.common.message.CustomErrorCode;
 import com.ocho.what2do.store.dto.StoreListResponseDto;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -18,38 +23,48 @@ import java.net.URI;
 @Component
 @RequiredArgsConstructor
 public class KakaoUtil {
-    private final RestTemplate restTemplate;
 
-    @Value("${kakao.Authorization}")
-    private String Authorization;
+  private final RestTemplate restTemplate;
 
-    //    @TimeTrace
-    public String callApi(String query, String page) {
-        // 요청 URL 만들기
-        URI uri = UriComponentsBuilder
-                .fromUriString("https://dapi.kakao.com")
-                .path("/v2/local/search/keyword.json")
-                .queryParam("query", query) // 검색어
-                .queryParam("page", page) // 페이징
-                .queryParam("size", "15") // 한 페이지에 출력 할 값의 갯수 (고정 값 설정)
-                .queryParam("category_group_code", "FD6") // FD6 음식점 코드 (음식점만 검색 결과로 나오게끔)
-                .encode()
-                .build()
-                .toUri();
-        log.info("uri = " + uri);
+  @Value("${kakao.Authorization}")
+  private String Authorization;
 
-        RequestEntity<Void> requestEntity = RequestEntity
-                .get(uri)
-                .header("Authorization", Authorization)
-                .build();
+  public static final String KAKAO_SEARCH_API = "https://dapi.kakao.com";
+  public static final String KAKAO_SEARCH_PATH = "v2/local/search/keyword.json";
 
-        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+  private static final String QUERY = "query";
+  private static final String PAGE = "page";
+  private static final String SIZE = "size";
+  private static final String AUTHORIZATION = "Authorization";
 
-        log.info("DAUM API Status Code : " + responseEntity.getStatusCode());
+  //    @TimeTrace
+  public String callApi(String query, String page) {
+    // 요청 URL 만들기
+    HttpHeaders headers = new HttpHeaders();
+    headers.set(AUTHORIZATION, Authorization);
 
-        if (Integer.parseInt(page) > 3) {
-            throw new CustomException(CustomErrorCode.DATA_NOT_FOUND);
-        }
-        return responseEntity.getBody();
+    // uri
+    UriComponents uriComponents = UriComponentsBuilder.fromUriString(KAKAO_SEARCH_API)
+        .path(KAKAO_SEARCH_PATH)
+        .queryParam(QUERY, query)
+        .queryParam(PAGE, page)
+        .queryParam(SIZE, 15)
+        .queryParam("category_group_code", "FD6") // FD6 음식점 코드 (음식점만 검색 결과로 나오게끔)
+        .encode()
+        .build();
+
+    // request
+    HttpEntity<?> entity = new HttpEntity<>(headers);
+
+    HttpEntity<String> response = restTemplate.exchange(
+        uriComponents.toUri(),
+        HttpMethod.GET,
+        entity,
+        String.class);
+
+    if (Integer.parseInt(page) > 3) {
+      throw new CustomException(CustomErrorCode.DATA_NOT_FOUND);
     }
+    return response.getBody();
+  }
 }
