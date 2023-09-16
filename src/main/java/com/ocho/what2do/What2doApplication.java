7cf26b7dcbc40c3cdb9a3e15f21ff88125db0844
sxi8022 <sxi8022@gmail.com>
2023-09-16 16:59:35 +0900

    [feature] 스케줄러로 이미지가 없는 가게는 이미지 추가

--- src/main/java/com/ocho/what2do/What2doApplication.java
+++ src/main/java/com/ocho/what2do/What2doApplication.java
@@ -2,9 +2,10 @@ package com.ocho.what2do;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling  //쓰지않을때 주석처리 배치프로그램
+@EnableScheduling  //쓰지않을때 주석처리 스케줄러
 public class What2doApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ocho/what2do/common/sceduler/StoreImageScheduler.java
+++ src/main/java/com/ocho/what2do/common/sceduler/StoreImageScheduler.java
@@ -1,12 +1,41 @@
 package com.ocho.what2do.common.sceduler;
 
+import com.ocho.what2do.store.service.ScheduleApiStoreServiceImpl;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j(topic = "StoreImage Scheduler")
 @Component
 @RequiredArgsConstructor
 public class StoreImageScheduler {
+
+  private final ScheduleApiStoreServiceImpl scheduleApiStoreService;
+
+  // @Scheduled(cron = "*/10 * * * * *", zone = "Asia/Seoul") //10 초마다
+  // 초, 분, 시, 일, 월, 주 순서
+  @Scheduled(cron = "0 0 18 * * *", zone = "Asia/Seoul") // 매일 새벽 1시
+  public void yourScheduledMethod() {
+    // 스케줄러가 실행될 로직
+    log.info("가게 이미지 등록 스케줄러 실행");
+    int result = 0;
+    try {
+      result = scheduleApiStoreService.UpdateApiStoreImages();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      if (result > 0) {
+        log.info("가게 이미지 등록 스케줄러 성공");
+      } else {
+        log.error("가게 이미지 등록 스케줄러 실패");
+      }
+    }
+
+
+  }
 
 }
